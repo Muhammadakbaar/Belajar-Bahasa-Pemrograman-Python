44ec8303c47024b097e3224cfc47d73158b0d225
Muhammad Akbar <sitamvan@sitamvan>
2020-07-30 11:57:51 +0700

    variabel dana tipe data

---
+++ VariabelDanTipeData.py
@@ -0,0 +1,19 @@
+# Belajar variabel dan tipe data
+
+# variabel = tempat untuk menyimpan suatu data atau nilai
+
+nama = "Hello World" # variabel nama dengan nilai hello world
+print (nama)
+
+nama = "akbar" # mengganti nilai nama dari hello world menjadi akbar
+print (nama)
+
+# jenis tipe data
+
+# - Numbers = int, float, complex
+# - String
+# - List
+# - Tuple
+# - Dictionary
+
+System.out.println() v
