17f01bbc8010b1ccfecf9ee34587370d7de86c84
Muhammad Akbar <sitamvan@sitamvan>
2020-07-30 12:41:56 +0700

    metode pada string

---
+++ TipeDataString.py
@@ -0,0 +1,31 @@
+# Tipe dat a string, metode, concinate dan escaping
+
+# menggabukan nilai variabel
+x = "Praxis"
+print(x + "Academy")
+
+y = "Academy"
+print(x +" "+ y)
+
+# menggunakan tanda petik        
+print('Akbar\' ganteng')
+
+# perpindahan baris
+print(" ini baris pertama \n ini baris kedua")
+
+#penggunaan TAB
+print("mencoba \t TAB ")
+
+# mengubah awal huruf menjadi kapital
+# cara pertama
+z = "akbar"
+j = z.capitalize()
+print(j)
+# cara kedua
+z = "akbar"
+print(z.capitalize())
+
+# menggantikan isi nilai
+a = "hakim"
+b = a.replace(a,"adam")
+print(b)
